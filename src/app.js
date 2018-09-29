4cc8cdbe226e78c2047010d2fd5859557f9a3550
John Grishin <r@exah.ru>
2018-09-29 17:38:45 +0300

    💄 Add `theme`

--- src/app.js
+++ src/app.js
@@ -1,14 +1,28 @@
 import { hot } from 'react-hot-loader'
+import { injectGlobal } from 'emotion'
+import { ThemeProvider } from 'emotion-theming'
 import React, { Component } from 'react'
+import { THEME } from './constants'
 import ExchangeView from './containers/exchange-view'
 
 class App extends Component {
+  constructor (props) {
+    super(props)
+
+    injectGlobal`
+      :root {
+        ${THEME.textStyle.root}
+      }
+    `
+  }
   render () {
     return (
-      <ExchangeView
-        defaultFromCurrency='USD' // this should come from user action
-        defaultToCurrency='GBP'
-      />
+      <ThemeProvider theme={THEME}>
+        <ExchangeView
+          defaultFromCurrency='USD' // this should come from user action
+          defaultToCurrency='GBP'
+        />
+      </ThemeProvider>
     )
   }
 }
--- src/constants/index.js
+++ src/constants/index.js
@@ -1,7 +1,40 @@
+export const DEBUG_SCOPE = 'exchange-widget'
+
 export const API_GET_RATES = '/api/rates/:currency'
 export const RATES_API_GET_LATEST = '/api/latest'
-
 export const EVENT_EXCHANGE_RATES_FOR_CURRENCY = 'exchange-rates-for-currency'
 export const EVENT_EXCHANGE_RATES = 'exchange-rates'
 
-export const DEBUG_SCOPE = 'exchange-widget'
+export const THEME_COLORS = {
+  text: '#000',
+  faded: 'rgba(0, 0, 0, 0.5)',
+  accent: '#1880EE',
+  highlight: '#E63FA5',
+  bg: 'rgba(0, 0, 0, 0.93)'
+}
+
+export const THEME_TEXT_STYLES = {
+  root: {
+    fontFamily: 'system-ui, sans-serif',
+    fontSize: 18,
+    lineHeight: 1.2,
+    fontWeight: 400
+  },
+  default: {
+    fontSize: '1rem',
+    fontWeight: 400
+  },
+  accent: {
+    fontSize: '1.4rem',
+    fontWeight: 500
+  },
+  caption: {
+    fontSize: '0.8rem',
+    fontWeight: 300
+  }
+}
+
+export const THEME = {
+  color: THEME_COLORS,
+  textStyle: THEME_TEXT_STYLES
+}
